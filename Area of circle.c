7c14d19214dd5114e3c6a00dba1ccb8bdf8bde07
ankitbharambe21 <86463400+ankitbharambe21@users.noreply.github.com>
2021-08-30 13:52:42 +0530

    Create Area of circle.c

---
+++ Area of circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  float r,AOC;
+  clrscr();
+  printf("Enter value of radius = ");
+  scanf("%f",&r);
+  printf("AOC=%.2f",3.14*r*r);
+  getch();
+}
